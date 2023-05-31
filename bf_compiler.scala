6914060b5f7020a706d5d8f09a4a54e1c5870fdc
Serge <syraj.alkhalil@kcl.ac.uk>
2023-05-31 15:50:10 +0100

    first commit

---
+++ bf_compiler.scala
@@ -0,0 +1,160 @@
+// A "Compiler" for the Brainfuck language
+//============================================================
+
+object M5b {
+
+// for timing purposes
+def time_needed[T](n: Int, code: => T) = {
+  val start = System.nanoTime()
+  for (i <- 0 until n) code
+  val end = System.nanoTime()
+  (end - start)/(n * 1.0e9)
+}
+
+type Mem = Map[Int, Int]
+
+import io.Source
+import scala.util._
+
+//====================== 
+// a couple of functions taken from bf_interpreter.scala
+def load_bff(name: String) : String = Try(Source.fromFile(name)("ISO-8859-1").mkString).getOrElse("")
+def sread(mem: Mem, mp: Int) : Int = mem.getOrElse(mp, 0)
+def write(mem: Mem, mp: Int, v: Int) : Mem = mem + (mp -> v)
+def jumpRight(prog: String, pc: Int, level: Int) : Int = { 
+    if (prog.length == pc) pc else {
+        prog match {
+            case p if p(pc) == '[' => jumpRight(prog, pc+1, level+1)
+            case p if p(pc) == ']' => if (level == 0) pc+1 else jumpRight(prog, pc+1, level - 1)
+            case _ => jumpRight(prog, pc+1, level)
+        }
+    }
+}
+def jumpLeft(prog: String, pc: Int, level: Int) : Int = {
+    if (pc < 0) pc else {
+        prog match {
+            case p if p(pc) == ']' => jumpLeft(prog, pc-1, level+1)
+            case p if p(pc) == '[' => if (level == 0) pc+1 else jumpLeft(prog, pc-1, level - 1)
+            case _ => jumpLeft(prog, pc-1, level)
+        }
+    }
+}
+//======================
+
+// (6)
+def jtable(pg: String) : Map[Int, Int] = jtable_helper(pg)
+
+def jtable_helper(pg: String, pc: Int = 0, ans: Map[Int, Int] = Map()) : Map[Int, Int] = {
+  if (pg.length == pc) ans else{
+    pg match {
+      case p if p(pc) == '[' => jtable_helper(pg, pc+1, (ans + (pc -> jumpRight(pg, pc+1, 0))) )
+      case p if p(pc) == ']' => jtable_helper(pg, pc+1, (ans + (pc -> jumpLeft(pg, pc-1, 0))) )
+      case _ => jtable_helper(pg, pc+1, ans)
+    }
+  }
+}
+
+def compute2(pg: String, tb: Map[Int, Int], pc: Int, mp: Int, mem: Mem) : Mem = {
+  if (pc < 0 || pc >= pg.length) mem else {
+        pg match {
+            case p if p(pc) == '>' => compute2(pg, tb, pc+1, mp+1, mem)
+            case p if p(pc) == '<' => compute2(pg, tb, pc+1, mp-1, mem)
+            case p if p(pc) == '+' => compute2(pg, tb, pc+1, mp, write(mem, mp, sread(mem, mp)+1))
+            case p if p(pc) == '-' => compute2(pg, tb, pc+1, mp, write(mem, mp, sread(mem, mp)-1))
+            case p if p(pc) == '.' => {
+                print(sread(mem, mp).toChar)
+                compute2(pg, tb, pc+1, mp, mem)
+            }
+            case p if p(pc) == '[' => if (sread(mem, mp) == 0) compute2(pg, tb, tb.get(pc).get, mp, mem) else compute2(pg, tb, pc+1, mp, mem)
+            case p if p(pc) == ']' => if (sread(mem, mp) != 0) compute2(pg, tb, tb.get(pc).get, mp, mem) else compute2(pg, tb, pc+1, mp, mem)
+            case _ => compute2(pg, tb, pc+1, mp, mem)
+        }
+    }
+}
+def run2(pg: String, m: Mem = Map()) = compute2(pg, jtable(pg), 0, 0, m)
+
+// (7) 
+def optimise(s: String) : String = s.replaceAll("""[^<>+\-.\[\]]""", "").replaceAll("""\[-\]""", "0")
+
+def compute3(pg: String, tb: Map[Int, Int], pc: Int, mp: Int, mem: Mem) : Mem = {
+  if (pc < 0 || pc >= pg.length) mem else {
+        pg match {
+            case p if p(pc) == '>' => compute3(pg, tb, pc+1, mp+1, mem)
+            case p if p(pc) == '<' => compute3(pg, tb, pc+1, mp-1, mem)
+            case p if p(pc) == '+' => compute3(pg, tb, pc+1, mp, write(mem, mp, sread(mem, mp)+1))
+            case p if p(pc) == '-' => compute3(pg, tb, pc+1, mp, write(mem, mp, sread(mem, mp)-1))
+            case p if p(pc) == '0' => compute3(pg, tb, pc+1, mp, write(mem, mp, 0))
+            case p if p(pc) == '.' => {
+                print(sread(mem, mp).toChar)
+                compute3(pg, tb, pc+1, mp, mem)
+            }
+            case p if p(pc) == '[' => if (sread(mem, mp) == 0) compute3(pg, tb, tb.get(pc).get, mp, mem) else compute3(pg, tb, pc+1, mp, mem)
+            case p if p(pc) == ']' => if (sread(mem, mp) != 0) compute3(pg, tb, tb.get(pc).get, mp, mem) else compute3(pg, tb, pc+1, mp, mem)
+            
+            case _ => compute3(pg, tb, pc+1, mp, mem)
+        }
+    }
+}
+
+def run3(pg: String, m: Mem = Map()) = compute3(optimise(pg), jtable(optimise(pg)), 0, 0, m)
+
+// (8)  
+def combine(s: String) : String = combine_helper(s, c=s(0))
+val good_stuff = List('[', ']', '0', '.')
+
+def combine_helper(string: String, pc: Int = 0, counter: Int = 0, c: Char) : String = {
+    if (pc == string.length) {
+        if (good_stuff.contains(string(pc-1))) string else command_gen(string, pc, counter, c)
+    } else {
+        string match {
+            case s if (good_stuff.contains(s(pc)) && counter == 0 )=> combine_helper(string, pc+1, 0, if (pc+1 != s.length) s(pc+1) else s(pc))
+            case s if s(pc) != c => {
+                val command = command_gen(string, pc, counter, c)
+                combine_helper(command + s.slice(pc, s.length), command.length, 0, s(pc))
+            }
+            case _ => combine_helper(string, pc+1, counter+1, c)
+        }
+    }
+}
+
+// probs rewrite this idk
+def command_gen(s: String, pc: Int, counter: Int, c: Char) : String = {
+    if (counter <= 26) {
+        s.slice(0, pc-counter) + c.toString + (64 + counter).toChar
+    }else{
+        val new_ptr = 26 + pc - counter
+        s.slice(0, pc-counter) + c.toString + "Z" + command_gen(s, pc - new_ptr, counter - 26, c)
+    }
+}
+
+
+def charToInt(char: Char) : Int = char.toInt - 64
+
+def compute4(pg: String, tb: Map[Int, Int], pc: Int, mp: Int, mem: Mem) : Mem = {
+  if (pc < 0 || pc >= pg.length) mem else {
+        pg match {
+            case p if p(pc) == '>' => compute4(pg, tb, pc+2, mp+charToInt(p(pc+1)), mem)
+            case p if p(pc) == '<' => compute4(pg, tb, pc+2, mp-charToInt(p(pc+1)), mem)
+            case p if p(pc) == '+' => compute4(pg, tb, pc+2, mp, write(mem, mp, sread(mem, mp)+charToInt(p(pc+1))))
+            case p if p(pc) == '-' => compute4(pg, tb, pc+2, mp, write(mem, mp, sread(mem, mp)-charToInt(p(pc+1))))
+            // should be unchanged
+            case p if p(pc) == '0' => compute4(pg, tb, pc+1, mp, write(mem, mp, 0))
+            case p if p(pc) == '.' => {
+                print(sread(mem, mp).toChar)
+                compute4(pg, tb, pc+1, mp, mem)
+            }
+            case p if p(pc) == '[' => if (sread(mem, mp) == 0) compute4(pg, tb, tb.get(pc).get, mp, mem) else compute4(pg, tb, pc+1, mp, mem)
+            case p if p(pc) == ']' => if (sread(mem, mp) != 0) compute4(pg, tb, tb.get(pc).get, mp, mem) else compute4(pg, tb, pc+1, mp, mem)
+            
+            case _ => compute4(pg, tb, pc+1, mp, mem)
+        }
+    }
+}
+
+// should call first optimise and then combine on the input string
+def run4(pg: String, m: Mem = Map()) = {
+    val new_pg = combine(optimise(pg))
+    compute4(new_pg, jtable(new_pg), 0, 0, m)
+}
+
+}
---
+++ bf_interpreter.scala
@@ -0,0 +1,64 @@
+// An Interpreter for the Brainfuck language
+//==============================================
+
+object M5a {
+
+// representation of BF memory 
+type Mem = Map[Int, Int]
+
+import io.Source
+import scala.util._
+
+// (1)
+def load_bff(name: String) : String = Try(Source.fromFile(name)("ISO-8859-1").mkString).getOrElse("")
+
+// (2) 
+def sread(mem: Mem, mp: Int) : Int = mem.getOrElse(mp, 0)
+
+def write(mem: Mem, mp: Int, v: Int) : Mem = mem + (mp -> v)
+
+// (3) 
+def jumpRight(prog: String, pc: Int, level: Int) : Int = { 
+    if (prog.length == pc) pc else {
+        prog match {
+            case p if p(pc) == '[' => jumpRight(prog, pc+1, level+1)
+            case p if p(pc) == ']' => if (level == 0) pc+1 else jumpRight(prog, pc+1, level - 1)
+            case _ => jumpRight(prog, pc+1, level)
+        }
+    }
+}
+
+def jumpLeft(prog: String, pc: Int, level: Int) : Int = {
+    if (pc < 0) pc else {
+        prog match {
+            case p if p(pc) == ']' => jumpLeft(prog, pc-1, level+1)
+            case p if p(pc) == '[' => if (level == 0) pc+1 else jumpLeft(prog, pc-1, level - 1)
+            case _ => jumpLeft(prog, pc-1, level)
+        }
+    }
+}
+
+// (4) 
+def compute(prog: String, pc: Int, mp: Int, mem: Mem) : Mem = {
+    if (pc < 0 || pc >= prog.length) mem else {
+        prog match {
+            case p if p(pc) == '>' => compute(prog, pc+1, mp+1, mem)
+            case p if p(pc) == '<' => compute(prog, pc+1, mp-1, mem)
+            case p if p(pc) == '+' => compute(prog, pc+1, mp, write(mem, mp, sread(mem, mp)+1))
+            case p if p(pc) == '-' => compute(prog, pc+1, mp, write(mem, mp, sread(mem, mp)-1))
+            case p if p(pc) == '.' => {
+                print(sread(mem, mp).toChar)
+                compute(prog, pc+1, mp, mem)
+            }
+            case p if p(pc) == '[' => if (sread(mem, mp) == 0) compute(prog, jumpRight(prog, pc+1, 0), mp, mem) else compute(prog, pc+1, mp, mem)
+            case p if p(pc) == ']' => if (sread(mem, mp) != 0) compute(prog, jumpLeft(prog, pc-1, 0), mp, mem) else compute(prog, pc+1, mp, mem)
+            case _ => compute(prog, pc+1, mp, mem)
+        }
+    }
+}
+
+def run(prog: String, m: Mem = Map()) = compute(prog, 0, 0, m)
+
+// (5)
+def generate(msg: List[Char]) : String = msg.map(c => ("+" * c.toInt) ++ ".[-]").mkString
+}
